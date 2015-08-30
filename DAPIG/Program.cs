760e690c4c968a1e065e4108993dc4517b35ece2
Dylan Dunn <robodylan123@gmail.com>
2015-08-30 11:50:59 -0400

    Made server multithreaded

--- DAPIG/Program.cs
+++ DAPIG/Program.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace DAPIG
 {
     internal class Program
     {
+        public static List<Entity> entities = new List<Entity>();
+        public static Random rand = new Random();
+        public static List<Tile> map = new List<Tile>();
         private static void Main(string[] args)
         {
-            Random rand = new Random();
-            List<Entity> entities = new List<Entity>();
-            List<Tile> map = new List<Tile>();
-            //Create tons of random entities
             TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 74);
             listener.Start();
-            TcpClient client = listener.AcceptTcpClient();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Thread thread = new Thread(handleClient);
+                thread.Start(client);
+                string output = "";
+                Console.WriteLine(output);
+            }
+            
+        }
+
+        public static void handleClient(object data)
+        {
+            TcpClient client = (TcpClient)data;
             NetworkStream stream = client.GetStream();
             int ID = rand.Next(111111111, 999999999);
-            entities.Add(new Entity(ID, "NOT_SET", 0, 0));
+            lock(entities) entities.Add(new Entity(ID, "NOT_SET", 0, 0));
             byte[] bufferTMP = Encoding.ASCII.GetBytes("ID:" + ID.ToString());
             stream.Write(bufferTMP, 0, bufferTMP.Length);
             while (client.Connected)
             {
                 byte[] buffer = new byte[1024];
+                Thread.Sleep(1000 / 60);
                 if (stream.DataAvailable)
                 {
                     stream.Read(buffer, 0, 1024);
@@ -32,15 +46,15 @@ namespace DAPIG
                     input = Encoding.ASCII.GetString(buffer);
                     input = input.Split(Convert.ToChar(0))[0];
                     switch (input.Split(':')[0])
-                    {                       
+                    {
                         case "getPlayers":
                             string output = "";
-                            foreach(Entity entity in entities)
+                            foreach (Entity entity in entities)
                             {
-                                output = output + "<" + entity.username + "," + entity.x + "," + entity.y + "," + entity.direction + "," + entity.health +">\r\n";
-                                buffer = Encoding.ASCII.GetBytes(output);
-                                stream.Write(buffer, 0, output.Length);
+                                lock (entities) output = output + "<" + entity.username + "," + entity.x + "," + entity.y + "," + entity.direction + "," + entity.health + ">\r\n";
                             }
+                            buffer = Encoding.ASCII.GetBytes(output);
+                            stream.Write(buffer, 0, output.Length);
                             break;
                         case "getMap":
                             //TODO: Return map
@@ -48,7 +62,8 @@ namespace DAPIG
                         case "moveForward":
                             foreach (Entity entity in entities)
                             {
-                                try {
+                                try
+                                {
                                     if (entity.key == Convert.ToInt32(input.Split(':')[1]))
                                     {
                                         entity.direction = Entity.Direction.Forward;
@@ -74,7 +89,6 @@ namespace DAPIG
                                 }
                                 catch
                                 {
-
                                 }
                             }
                             break;
@@ -130,7 +144,7 @@ namespace DAPIG
                             }
                             break;
                         case "setUsername":
-                            foreach(Entity entity in entities)
+                            foreach (Entity entity in entities)
                             {
                                 try
                                 {
@@ -140,9 +154,9 @@ namespace DAPIG
                                         break;
                                     }
                                 }
-                                    catch
+                                catch
                                 {
-                           
+
                                 }
                             }
                             break;
@@ -155,7 +169,7 @@ namespace DAPIG
                                     break;
                                 }
                             }
-                            break; 
+                            break;
                     }
 
                 }
