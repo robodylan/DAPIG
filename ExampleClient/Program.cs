c99ad571b33b21e6a23eefc5d24f0ff42442c632
Dylan Dunn <robodylan123@gmail.com>
2015-08-30 20:53:45 -0400

    Added example client project

---
+++ ExampleClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ExampleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 74);
+            client.Connect();
+        }
+    }
+}
