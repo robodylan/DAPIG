71b4f085175640297924b27b5152449626ae73db
Dylan Dunn <robodylan123@gmail.com>
2015-08-30 22:02:05 -0400

    Added data processing to example client

--- DAPIG/ExampleClient/Game1.cs
+++ DAPIG/ExampleClient/Game1.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,9 +11,15 @@ namespace ExampleClient
 {
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-
+        public static byte[] buffer = new byte[1024];
+        public static string key;
+        public static Texture2D entityTexture;
+        public static List<Tile> map = new List<Tile>();
+        public static List<Entity> entities = new List<Entity>();
+        public static GraphicsDeviceManager graphics;
+        public static SpriteBatch spriteBatch;
+        public static TcpClient client = new TcpClient("127.0.0.1", 74);
+        public static NetworkStream stream = client.GetStream();
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -17,7 +28,15 @@ namespace ExampleClient
         
         protected override void Initialize()
         {
-          base.Initialize();
+            string input;
+            while (!stream.DataAvailable);
+            stream.Read(buffer, 0, buffer.Length);
+            input = Encoding.ASCII.GetString(buffer);
+            input = input.Split(Convert.ToChar(0))[0];
+            input = stripEnding(input);
+            key = input.Split(':')[1];
+            Console.WriteLine(key);
+            base.Initialize();
         }
         
         protected override void LoadContent()
@@ -32,17 +51,47 @@ namespace ExampleClient
         
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-            
+            entities.Clear();
+            string output = "getPlayers:";
+            buffer = Encoding.ASCII.GetBytes(output);
+            stream.Write(buffer, 0, output.Length);
+            while (!stream.DataAvailable);
+            stream.Read(buffer, 0, buffer.Length);
+            string input = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
+            string[] players = input.Split('\r');
+            foreach(string player in players)
+            {
+                Entity tmpEntity = new Entity("robodylan", 0, 0);
+                string[] properties = player.Split(',');
+                tmpEntity.x = Convert.ToInt32(properties[1]);
+                tmpEntity.y = Convert.ToInt32(properties[2]);
+            }
             base.Update(gameTime);
         }
         
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            spriteBatch.Begin();
+            foreach(Entity entity in entities)
+            {
+                spriteBatch.Draw(entityTexture, new Vector2(entity.x, entity.y), Color.White);
+            }
+            spriteBatch.End();
             base.Draw(gameTime);
+        }
+        public static string stripEnding(string input)
+        {
+            string output;
+            if (input.ToCharArray()[input.Length - 1] == '\n')
+            {
+                output = input.Substring(0, input.Length - 2); ;
+            }
+            else
+            {
+                output = input;
+            }
+            return output;
         }
     }
 }
--- DAPIG/ExampleClient/Program.cs
+++ DAPIG/ExampleClient/Program.cs
@@ -3,19 +3,13 @@
 namespace ExampleClient
 {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+        public static Game1 game;
         static void Main()
         {
-            using (var game = new Game1())
-                game.Run();
+            game = new Game1();
+            game.Run();
         }
     }
 #endif
